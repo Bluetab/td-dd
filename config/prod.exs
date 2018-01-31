00705d5620494918662254f061a1c1525056fd62
MSL <manuel.sierra@bluetab.net>
2018-01-31 15:41:08 +0100

    distillery config

--- config/prod.exs
+++ config/prod.exs
@@ -16,6 +16,8 @@ use Mix.Config
 config :data_quality, DataQualityWeb.Endpoint,
   load_from_system_env: true,
   url: [host: "example.com", port: 80],
+  server: true,
+  version: Mix.Project.config[:version],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
