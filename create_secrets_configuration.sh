d338f3cd7e00cc545eb776c87137809fcf19af86
ICBluetab <inigo.crespo@bluetab.net>
2018-02-22 17:55:17 +0100

    Fix secrets configuration

--- create_secrets_configuration.sh
+++ create_secrets_configuration.sh
@@ -3,4 +3,4 @@ sed -i -e "s/username:.*,/username: \"$DB_USER\",/g" ./config/prod.secret.exs
 sed -i -e "s/password:.*,/password: \"$DB_PASSWORD\",/g" ./config/prod.secret.exs
 sed -i -e "s/database:.*,/database: \"$DB_NAME\",/g" ./config/prod.secret.exs
 sed -i -e "s/hostname:.*,/hostname: \"$DB_HOST\",/g" ./config/prod.secret.exs
-cp ./config/prod.secret.exs ~/dataquality.prod.secret.exs
+cp ./config/prod.secret.exs ~/datadictionary.prod.secret.exs
