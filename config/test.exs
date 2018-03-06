b7476829539ad8793f9057727c6bedee9573a5fc
MSL <manuel.sierra@bluetab.net>
2018-03-06 17:26:00 +0100

    update test port

--- config/test.exs
+++ config/test.exs
@@ -3,7 +3,7 @@ use Mix.Config
 # We don't run a server during test. If one is required,
 # you can enable the server option below.
 config :td_dd, TdDDWeb.Endpoint,
-  http: [port: 4001],
+  http: [port: 3005],
   server: true
 
 
