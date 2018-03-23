88aecb42c1e16e3fd2943d23d80016eab26e1146
ICBluetab <inigo.crespo@bluetab.net>
2018-03-23 14:12:29 +0100

    Test pipeline

--- ci/integration.sh
+++ ci/integration.sh
@@ -1,25 +1,76 @@
 #!/bin/bash
 
+function wait_for {
+  while ! nc -z $1 $2; do
+    echo "$1 is unavailable - sleeping"
+    sleep 1
+  done
+}
+
 service postgresql96 start
+  
+git clone git@gitlab.bluetab.net:dgs-core/true-dat/back-end/td-auth.git /td_auth
+#git clone git@gitlab.bluetab.net:dgs-core/true-dat/back-end/td-bg.git /td_bg
+#git clone git@gitlab.bluetab.net:dgs-core/true-dat/back-end/td-dl.git /td_dl
+#git clone git@gitlab.bluetab.net:dgs-core/true-dat/back-end/td-dq.git /td_dq
+git clone git@gitlab.bluetab.net:dgs-core/true-dat/back-end/td-dd.git /td_dd
+git clone git@gitlab.bluetab.net:dgs-core/true-dat/td-int.git /td_int
 
-cp -R /code /working_code
-cd /working_code
-
-echo "Starting test step"
-
-export MIX_ENV=test
-
-echo "Starting prebuild configuration"
-mix local.hex --force
-echo "local hex executed"
+cd /td_auth
 mix local.rebar --force
 mix deps.clean --all
-echo "local rebar executed"
-echo "Downloading deps"
 mix deps.get
-echo "Executing credo"
-mix credo --strict || exit 1
-echo "Starting tests"
-mix test || exit 1
 
-echo "Test step finish successfully"
+if [ -d "assets" ]; then
+  cd assets
+  yarn install
+  cd ..
+fi
+
+mix ecto.create && mix ecto.migrate
+nohup mix phx.server & 
+
+cd /td_bg
+mix local.rebar --force
+mix deps.clean --all
+mix deps.get
+
+if [ -d "assets" ]; then
+  cd assets
+  yarn install
+  cd ..
+fi
+
+mix ecto.create && mix ecto.migrate
+nohup mix phx.server & 
+
+cd /td_dd
+mix local.rebar --force
+mix deps.clean --all
+mix deps.get
+
+if [ -d "assets" ]; then
+  cd assets
+  yarn install
+  cd ..
+fi
+
+mix ecto.create && mix ecto.migrate
+nohup mix phx.server & 
+
+yum install nc -y
+yum install python36-virtualenv -y
+
+virtualenv-3.6 venv
+. ./venv/bin/activate
+pip3.6 install -r requirements.txt
+
+wait_for localhost 4001
+#wait_for td_bg   4002
+#wait_for td_dl   4003
+#wait_for td_dq   4004
+wait_for localhost 4005
+
+cd /td_int/tests && chmod +x run_tests.sh && ./run_tests.sh
+
+
