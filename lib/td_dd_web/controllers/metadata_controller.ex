9da52f3b3302928b1213fda76067a538db0a2abe
ICBluetab <inigo.crespo@bluetab.net>
2018-05-31 15:48:29 +0200

    Add last_change_at to metadata

--- lib/td_dd_web/controllers/metadata_controller.ex
+++ lib/td_dd_web/controllers/metadata_controller.ex
@@ -13,7 +13,7 @@ defmodule TdDdWeb.MetadataController do
                         "type",
                         "ou",
                         "lopd",
-                        "modifications"]
+                        "metadata"]
 
   @data_field_keys ["system",
                     "group",
@@ -24,7 +24,7 @@ defmodule TdDdWeb.MetadataController do
                     "nullable",
                     "precision",
                     "business_concept_id",
-                    "modifications"]
+                    "metadata"]
 
   @data_structure_query  """
     INSERT INTO data_structures ("system", "group", "name", description, type, ou, lopd, metadata, last_change_at, last_change_by, inserted_at, updated_at)
@@ -92,10 +92,11 @@ defmodule TdDdWeb.MetadataController do
     |> File.stream!
     |> CSV.decode!(separator: ?;, headers: true)
     |> Enum.each(fn(data) ->
+      last_change_at = DateTime.utc_now()
       input = data
-      |> add_metadata(["description"])
+      |> add_metadata(["description", "ou", "lopd"], last_change_at)
       |> to_array(data_structure_keys)
-      |> add_user_and_date_time(conn)
+      |> add_user_and_date_time(conn, last_change_at)
       SQL.query!(Repo, @data_structure_query, input)
     end)
 
@@ -107,10 +108,11 @@ defmodule TdDdWeb.MetadataController do
     |> File.stream!
     |> CSV.decode!(separator: ?;, headers: true)
     |> Enum.each(fn(data) ->
+      last_change_at = DateTime.utc_now()
       input = data
-      |> add_metadata(["description", "ou", "lopd", "last_change_at"])
+      |> add_metadata(["description"], last_change_at)
       |> to_array(data_field_keys)
-      |> add_user_and_date_time(conn)
+      |> add_user_and_date_time(conn, last_change_at)
       SQL.query!(Repo, @data_field_query, input)
     end)
 
@@ -129,13 +131,14 @@ defmodule TdDdWeb.MetadataController do
   end
   defp get_value(data, name), do: Map.get(data, name)
 
-  defp add_user_and_date_time(data, conn) do
-    data ++ [GuardianPlug.current_resource(conn).id, DateTime.utc_now()]
+  defp add_user_and_date_time(data, conn, last_change_at) do
+    data ++ [GuardianPlug.current_resource(conn).id, last_change_at]
   end
 
- defp add_metadata(data, fields) do
+ defp add_metadata(data, fields, last_change_at) do
    metadata = fields
    |> Enum.reduce(%{}, &Map.put(&2, &1, Map.get(data, &1)))
+   |> Map.put("last_change_at", last_change_at)
    Map.put(data, "metadata", metadata)
  end
 
