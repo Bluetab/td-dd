7413cc5cec3f97b9ee5393743e3ad437093d335c
ICBluetab <inigo.crespo@bluetab.net>
2018-05-31 16:19:28 +0200

    Extract metadata configuration to its own file

---
+++ config/metadata.exs
@@ -0,0 +1,30 @@
+use Mix.Config
+
+config :td_dd, :metadata,
+  data_structure_keys: ["system", "group", "name", "description", "type", "ou",
+                        "lopd", "metadata"],
+  data_field_keys: ["system", "group", "name",
+                    "field_name",
+                    "type",
+                    "description",
+                    "nullable",
+                    "precision",
+                    "business_concept_id",
+                    "metadata"],
+  data_structure_query:
+  """
+    INSERT INTO data_structures ("system", "group", "name", description, type, ou, lopd, metadata, last_change_at, last_change_by, inserted_at, updated_at)
+    VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $10, $9, $10, $10)
+    ON CONFLICT ("system", "group", "name")
+    DO UPDATE SET type = $5, last_change_at = $10, last_change_by = $9, updated_at = $10;
+  """,
+  data_field_query:
+  """
+    INSERT INTO data_fields (data_structure_id, name, type, description, nullable, precision, business_concept_id, metadata, last_change_at, last_change_by, inserted_at, updated_at)
+    VALUES ((select id from data_structures where "system" = $1 and "group" = $2 and "name" = $3),
+    $4, $5, $6, $7, $8, $9, $10, $12, $11, $12, $12)
+    ON CONFLICT (data_structure_id, name)
+    DO UPDATE SET name = $4, type = $5, nullable = $7, precision = $8, business_concept_id = $9, last_change_at = $12, last_change_by = $11, updated_at = $12
+  """,
+  data_structure_modifiable_fields: ["description", "ou", "lopd"],
+  data_field_modifiable_fields: ["description"]
--- lib/td_dd_web/controllers/metadata_controller.ex
+++ lib/td_dd_web/controllers/metadata_controller.ex
@@ -6,40 +6,12 @@ defmodule TdDdWeb.MetadataController do
   alias TdDd.Repo
   alias TdDd.Auth.Guardian.Plug, as: GuardianPlug
 
-  @data_structure_keys ["system",
-                        "group",
-                        "name",
-                        "description",
-                        "type",
-                        "ou",
-                        "lopd",
-                        "metadata"]
-
-  @data_field_keys ["system",
-                    "group",
-                    "name",
-                    "field_name",
-                    "type",
-                    "description",
-                    "nullable",
-                    "precision",
-                    "business_concept_id",
-                    "metadata"]
-
-  @data_structure_query  """
-    INSERT INTO data_structures ("system", "group", "name", description, type, ou, lopd, metadata, last_change_at, last_change_by, inserted_at, updated_at)
-    VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $10, $9, $10, $10)
-    ON CONFLICT ("system", "group", "name")
-    DO UPDATE SET type = $5, last_change_at = $10, last_change_by = $9, updated_at = $10;
-  """
-
-  @data_field_query  """
-    INSERT INTO data_fields (data_structure_id, name, type, description, nullable, precision, business_concept_id, metadata, last_change_at, last_change_by, inserted_at, updated_at)
-    VALUES ((select id from data_structures where "system" = $1 and "group" = $2 and "name" = $3),
-    $4, $5, $6, $7, $8, $9, $10, $12, $11, $12, $12)
-    ON CONFLICT (data_structure_id, name)
-    DO UPDATE SET name = $4, type = $5, nullable = $7, precision = $8, business_concept_id = $9, last_change_at = $12, last_change_by = $11, updated_at = $12
-  """
+  @data_structure_keys Application.get_env(:td_dd, :metadata)[:data_structure_keys]
+  @data_field_keys Application.get_env(:td_dd, :metadata)[:data_field_keys]
+  @data_structure_query Application.get_env(:td_dd, :metadata)[:data_structure_query]
+  @data_field_query Application.get_env(:td_dd, :metadata)[:data_field_query]
+  @data_structure_modifiable_fields Application.get_env(:td_dd, :metadata)[:data_structure_modifiable_fields]
+  @data_field_modifiable_fields Application.get_env(:td_dd, :metadata)[:data_field_modifiable_fields]
 
   @data_structures_param "data_structures"
 
@@ -94,7 +66,7 @@ defmodule TdDdWeb.MetadataController do
     |> Enum.each(fn(data) ->
       last_change_at = DateTime.utc_now()
       input = data
-      |> add_metadata(["description", "ou", "lopd"], last_change_at)
+      |> add_metadata(@data_structure_modifiable_fields, last_change_at)
       |> to_array(data_structure_keys)
       |> add_user_and_date_time(conn, last_change_at)
       SQL.query!(Repo, @data_structure_query, input)
@@ -110,7 +82,7 @@ defmodule TdDdWeb.MetadataController do
     |> Enum.each(fn(data) ->
       last_change_at = DateTime.utc_now()
       input = data
-      |> add_metadata(["description"], last_change_at)
+      |> add_metadata(@data_field_modifiable_fields, last_change_at)
       |> to_array(data_field_keys)
       |> add_user_and_date_time(conn, last_change_at)
       SQL.query!(Repo, @data_field_query, input)
