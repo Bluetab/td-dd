1f323ec8e78a06e6a02e1e8901c116724dcf5fce
Nacho <ignacio.hidalgo@bluetab.net>
2018-07-04 12:08:56 +0200

    delete build_centos

--- build_centos/create_secrets_configuration.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-sed -i -e "s/username:.*,/username: \"$DB_USER\",/g" ./config/prod.secret.exs
-sed -i -e "s/password:.*,/password: \"$DB_PASSWORD\",/g" ./config/prod.secret.exs
-sed -i -e "s/database:.*,/database: \"$DB_NAME\",/g" ./config/prod.secret.exs
-sed -i -e "s@secret_key:.*@secret_key: \"$GUARDIAN_SECRET_KEY\"@g" ./config/prod.secret.exs
-sed -i -e "s/auth_host:.*,/auth_host: \"$API_AUTH_HOST\",/g" ./config/prod.secret.exs
-sed -i -e "s/auth_port:.*,/auth_port: \"$API_AUTH_PORT\",/g" ./config/prod.secret.exs
-sed -i -e "s/api_username:.*,/api_username: \"$API_USER\",/g" ./config/prod.secret.exs
-sed -i -e "s/api_password:.*/api_password: \"$API_PASSWORD\"/g" ./config/prod.secret.exs
--- build_centos/run_build.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-docker run --rm -v $(pwd):/code:ro -v $(pwd)/dist:/code/dist -w /code --entrypoint=/code/build_centos/entrypoint.sh nachohidalgo89/centos-phoenix:20180508124817
