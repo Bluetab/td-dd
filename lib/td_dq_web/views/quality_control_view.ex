6c9a668158b1e4f2a6472793250592ad2c8350b1
Luis Sebastián Huerta <luis.sebastian@bluetab.net>
2018-08-06 09:35:04 +0200

    fix: wrong cache searching for concept name

--- lib/td_dq_web/views/quality_control_view.ex
+++ lib/td_dq_web/views/quality_control_view.ex
@@ -2,7 +2,7 @@ defmodule TdDqWeb.QualityControlView do
   use TdDqWeb, :view
   alias TdDqWeb.QualityControlView
   alias TdDqWeb.QualityRuleView
-  alias TdPerms.TaxonomyCache
+  alias TdPerms.BusinessConceptCache
 
   def render("index.json", %{quality_controls: quality_controls}) do
     %{data: render_many(quality_controls, QualityControlView, "quality_control.json")}
@@ -14,7 +14,7 @@ defmodule TdDqWeb.QualityControlView do
 
   def render("quality_control.json", %{quality_control: quality_control}) do
     %{id: quality_control.id,
-      business_concept_name: TaxonomyCache.get_name(quality_control.business_concept_id),
+      business_concept_name: BusinessConceptCache.get_name(quality_control.business_concept_id),
       business_concept_id: quality_control.business_concept_id,
       name: quality_control.name,
       description: quality_control.description,
