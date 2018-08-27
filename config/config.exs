311840653168ae80f9188fb16f6bca1d993d28ab
saul.isern@bluetab.net <saul.isern@bluetab.net>
2018-08-27 12:29:53 +0200

    chore: Modifying create_quality_rule permission to manage_quality_rule

--- config/config.exs
+++ config/config.exs
@@ -72,7 +72,7 @@ config :td_perms, permissions: [
     :view_rejected_business_concepts,
     :view_deprecated_business_concepts,
     :manage_business_concept_links,
-    :create_quality_rule
+    :manage_quality_rule
   ]
 
 import_config "metadata.exs"
