d62693d99ed6a40f4ed1dffe629f3573c86e3ff1
saul.isern@bluetab.net <saul.isern@bluetab.net>
2018-09-06 16:59:36 +0200

    chore: Update seeds to generic type

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -31,7 +31,7 @@ qrts = [
   %QualityRuleType{name: "max_date", params: %{"type_params": [%{"name": "max_date", "type": "date"}], "system_params": [%{"name": "table", "type": "string"}, %{"name": "column", "type": "string"}]}},
   %QualityRuleType{name: "min_date", params: %{"type_params": [%{"name": "min_date", "type": "date"}], "system_params": [%{"name": "table", "type": "string"}, %{"name": "column", "type": "string"}]}},
   %QualityRuleType{name: "dates_range", params: %{"type_params": [%{"name": "min_date", "type": "date"}, %{"name": "max_date", "type": "date"}], "system_params": [%{"name": "table", "type": "string"}, %{"name": "column", "type": "string"}]}},
-  %QualityRuleType{name: "custom_validation"}
+  %QualityRuleType{name: "custom_validation", params: %{}}
 ]
 
 for qrt <- qrts do
