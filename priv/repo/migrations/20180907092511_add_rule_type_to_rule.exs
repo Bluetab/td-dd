47c2a3033860ffffa7923b749d772150c3b7dcc7
ICBluetab <inigo.crespo@bluetab.net>
2018-09-10 17:16:42 +0200

    refactor: fix migration

--- priv/repo/migrations/20180907092511_add_rule_type_to_rule.exs
+++ priv/repo/migrations/20180907092511_add_rule_type_to_rule.exs
@@ -1,14 +1,14 @@
 defmodule TdDq.Repo.Migrations.AddRuleTypeToRule do
   use Ecto.Migration
-  # alias Ecto.Adapters.SQL
-  # alias TdDd.Repo
+  alias Ecto.Adapters.SQL
+  alias TdDq.Repo
 
   def up do
     alter table(:rules), do: add :rule_type_id, references(:rule_types), null: true
 
     flush()
 
-    #SQL.query!(Repo, "update rules set rule_type_id = (select id from rule_types where name = type)")
+    SQL.query!(Repo, "update rules set rule_type_id = (select id from rule_types as rt where rt.name = type)")
 
     alter table(:rules), do: modify :rule_type_id, :bigint, null: false
     rename table(:rules), :type, to: :type_backup
