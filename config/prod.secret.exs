e05775f294834cc0bc959b5ce28e528d85bce3ad
Nacho <ignacio.hidalgo@bluetab.net>
2018-09-11 16:37:51 +0200

    fix: integer timeout

--- config/prod.secret.exs
+++ config/prod.secret.exs
@@ -19,7 +19,7 @@ config :td_dd, TdDd.Repo,
   database: "${DB_NAME}",
   hostname: "${DB_HOST}",
   pool_size: 10,
-  timeout: {:integer, "${DB_TIMEOUT}"}
+  timeout: String.to_integer("${DB_TIMEOUT}")
 
 config :td_dd, TdDd.Auth.Guardian,
   allowed_algos: ["HS512"], # optional
