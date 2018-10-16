64ccaca205deb5e0f95988c836e08e8a2a5bfb2d
ICBluetab <inigo.crespo@bluetab.net>
2018-10-16 17:04:32 +0200

    fix: change permission name

--- config/config.exs
+++ config/config.exs
@@ -36,7 +36,7 @@ config :td_perms, permissions: [
   :view_deprecated_business_concepts,
   :manage_business_concept_links,
   :manage_quality_rule,
-  :view_confidential_business_concepts
+  :manage_confidential_business_concepts
 ]
 
 config :td_dq, cache_rules_on_startup: true
