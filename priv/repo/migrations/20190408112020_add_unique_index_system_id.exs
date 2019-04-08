9bd8251d054cb6d09db9bc045952450c57e08b8b
Tom Crossland <tom.crossland@bluetab.net>
2019-04-08 13:22:51 +0200

    fix: update unique index to use system_id instead of system

---
+++ priv/repo/migrations/20190408112020_add_unique_index_system_id.exs
@@ -0,0 +1,8 @@
+defmodule TdDd.Repo.Migrations.AddUniqueIndexSystemIdGroupNameExternalId do
+  use Ecto.Migration
+
+  def change do
+    drop(index(:data_structures, [:system, :group, :name, :external_id]))
+    create(unique_index(:data_structures, [:system_id, :group, :name, :external_id]))
+  end
+end
