d58093df648a478de85842e44e35e0e19701df4f
tom.crossland@bluetab.net <tom.crossland@bluetab.net>
2019-04-08 09:49:41 +0000

    [TD-1593] fix issues with migrations

--- priv/repo/migrations/20190405000030_link_structures_to_systems.exs
+++ priv/repo/migrations/20190405000030_link_structures_to_systems.exs
@@ -4,17 +4,12 @@ defmodule TdDd.Repo.Migrations.LinkStructuresToSystems do
   import Ecto.Query
 
   alias TdDd.Repo
+  alias TdDd.Systems.System
 
   def up do
-    from(
-      ds in "data_structures",
-      update: [set: [system: "unknown"]],
-      where: is_nil(ds.system)
-    )
-    |> Repo.update_all([])
-
-    from(s in "systems", select: %{id: s.id, external_id: s.external_id})
+    System
     |> Repo.all()
+    |> Enum.map(&Map.take(&1, [:name, :id]))
     |> Enum.each(&update_system_id/1)
   end
 
@@ -24,20 +19,13 @@ defmodule TdDd.Repo.Migrations.LinkStructuresToSystems do
       update: [set: [system_id: nil]]
     )
     |> Repo.update_all([])
-
-    from(
-      ds in "data_structures",
-      update: [set: [system: nil]],
-      where: ds.system == "unknown"
-    )
-    |> Repo.update_all([])
   end
 
-  defp update_system_id(%{id: id, external_id: external_id}) do
+  defp update_system_id(%{id: id, name: name}) do
     from(
       ds in "data_structures",
       update: [set: [system_id: ^id]],
-      where: ds.system == ^external_id
+      where: ds.system == ^name
     )
     |> Repo.update_all([])
   end
