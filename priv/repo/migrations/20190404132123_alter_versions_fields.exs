dc229d9a96f4c0e9166a3a9ed06f203f74687a3e
Tom Crossland <tom.crossland@bluetab.net>
2019-04-10 13:28:47 +0200

    feat: [TD-1597] cascade deletes on versions_fields

---
+++ priv/repo/migrations/20190404132123_alter_versions_fields.exs
@@ -0,0 +1,29 @@
+defmodule TdDd.Repo.Migrations.AlterVersionsFields do
+  use Ecto.Migration
+
+  def up do
+    execute(
+      "ALTER TABLE versions_fields DROP CONSTRAINT versions_fields_data_structure_version_id_fkey"
+    )
+
+    execute("ALTER TABLE versions_fields DROP CONSTRAINT versions_fields_data_field_id_fkey")
+
+    alter table("versions_fields") do
+      modify(:data_structure_version_id, references(:data_structures, on_delete: :delete_all))
+      modify(:data_field_id, references(:data_fields, on_delete: :delete_all))
+    end
+  end
+
+  def down do
+    execute(
+      "ALTER TABLE versions_fields DROP CONSTRAINT versions_fields_data_structure_version_id_fkey"
+    )
+
+    execute("ALTER TABLE versions_fields DROP CONSTRAINT versions_fields_data_field_id_fkey")
+
+    alter table("versions_fields") do
+      modify(:data_structure_version_id, references(:data_structures, on_delete: :nothing))
+      modify(:data_field_id, references(:data_fields, on_delete: :nothing))
+    end
+  end
+end
