2560a60161685d95f93a6dcf6abe461a2a3c0b9c
Tom Crossland <tom.crossland@bluetab.net>
2019-04-11 16:36:49 +0200

    feat: [TD-1626] delete structures with duplicate external_id within a system

---
+++ priv/repo/migrations/20190411134445_create_partial_index_system_id_external_id.exs
@@ -0,0 +1,15 @@
+defmodule TdDd.Repo.Migrations.CreatePartialIndexSystemIdExternalId do
+  use Ecto.Migration
+
+  def change do
+    drop(unique_index("data_structures", [:system_id, :group, :name, :external_id]))
+
+    create(
+      unique_index("data_structures", [:system_id, :external_id], where: "external_id IS NOT NULL")
+    )
+
+    create(
+      unique_index("data_structures", [:system_id, :group, :name], where: "external_id IS NULL")
+    )
+  end
+end
