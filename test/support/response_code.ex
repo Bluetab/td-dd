e560c1e51976db1bbadc20dfd1351922972575f4
Tom Crossland <tom.crossland@bluetab.net>
2019-05-10 15:38:03 +0200

    chore: disable credo warning

--- test/support/response_code.ex
+++ test/support/response_code.ex
@@ -11,12 +11,14 @@ defmodule TdDdWeb.ResponseCode do
   @unprocessable_entity "Unprocessable Entity"
 
   def rc_ok, do: @ok
+  def rc_accepted, do: @accepted
   def rc_created, do: @created
   def rc_forbidden, do: @forbidden
   def rc_unauthorized, do: @unauthorized
   def rc_not_found, do: @not_found
   def rc_unprocessable_entity, do: @unprocessable_entity
 
+  # credo:disable-for-next-line Credo.Check.Refactor.CyclomaticComplexity
   def to_response_code(http_status_code) do
     case http_status_code do
       200 -> @ok
