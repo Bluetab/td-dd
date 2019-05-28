a0ca387a670f13e1bc92edd28a521309911e1a03
Tom Crossland <tom.crossland@bluetab.net>
2019-05-28 10:50:01 +0200

    doc: changelog, version 2.20.1

--- mix.exs
+++ mix.exs
@@ -6,7 +6,7 @@ defmodule TdDd.Mixfile do
       app: :td_dd,
       version:
         case System.get_env("APP_VERSION") do
-          nil -> "2.20.0-local"
+          nil -> "2.20.1-local"
           v -> v
         end,
       elixir: "~> 1.6",
