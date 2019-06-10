16e3f9af6eabafd1b09144df5b4e62b58821f153
saul.isern <saul.isern@bluetab.net>
2019-06-10 15:17:58 +0200

    chore: New release

--- mix.exs
+++ mix.exs
@@ -6,7 +6,7 @@ defmodule TdDq.Mixfile do
       app: :td_dq,
       version:
         case System.get_env("APP_VERSION") do
-          nil -> "2.20.0-local"
+          nil -> "2.21.0-local"
           v -> v
         end,
       elixir: "~> 1.6",
