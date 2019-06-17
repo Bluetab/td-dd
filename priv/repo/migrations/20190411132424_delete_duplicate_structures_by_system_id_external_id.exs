967a081d223e73fe35ece186b399914d7ca70cdc
saul.isern <saul.isern@bluetab.net>
2019-06-17 12:34:34 +0200

    fix: [TD-1866] use delete_all instead of delete

--- priv/repo/migrations/20190411132424_delete_duplicate_structures_by_system_id_external_id.exs
+++ priv/repo/migrations/20190411132424_delete_duplicate_structures_by_system_id_external_id.exs
@@ -5,13 +5,19 @@ defmodule TdDd.Repo.Migrations.DeleteDuplicateStructuresBySystemIdExternalId do
   alias TdDd.Repo
 
   def change do
-    from(ds in "data_structures", 
-      select: %{external_id: ds.external_id, system_id: ds.system_id})
+    from(ds in "data_structures")
+    |> select([ds], %{id: ds.id, external_id: ds.external_id, system_id: ds.system_id})
     |> Repo.all()
     |> Enum.filter(&(not is_nil(&1.external_id)))
     |> Enum.group_by(&{&1.system_id, &1.external_id})
     |> Enum.filter(fn {_, dss} -> Enum.count(dss) > 1 end)
     |> Enum.flat_map(fn {_, [_h | t]} -> t end)
-    |> Enum.map(&Repo.delete/1)
+    |> Enum.map(&delete_structure/1)
+  end
+
+  defp delete_structure(%{id: id}) do
+     from(ds in "data_structures")
+     |> where([ds], ds.id == ^id) 
+     |> Repo.delete_all()
   end
 end
