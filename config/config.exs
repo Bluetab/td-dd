f64c9195e84ddb107a347c3695878304d43b122d
Tom Crossland <tom.crossland@bluetab.net>
2019-06-27 13:20:22 +0200

    fix: configure Jason for Phoenix

--- config/config.exs
+++ config/config.exs
@@ -91,6 +91,9 @@ config :logger, :console,
   format: (System.get_env("EX_LOGGER_FORMAT") || "$time $metadata[$level] $message") <> "\n",
   metadata: [:request_id]
 
+# Configuration for Phoenix
+config :phoenix, :json_library, Jason
+
 config :td_dq, :phoenix_swagger,
   swagger_files: %{
     "priv/static/swagger.json" => [router: TdDqWeb.Router]
