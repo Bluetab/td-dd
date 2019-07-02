f67b71c2614d35455cdd5ee33d65883166d824ee
sc <sonia.curiel@bluetab.net>
2019-07-02 14:30:19 +0200

    change: [TD-1687] soft deletion of rule implementations

---
+++ priv/repo/migrations/20190702094014_add_deleted_at_in_rule_implementation.exs
@@ -0,0 +1,16 @@
+defmodule TdDq.Repo.Migrations.AddDeletedAtInRuleImplementation do
+  use Ecto.Migration
+
+  def up do
+    alter(table(:rule_implementations), do: add(:deleted_at, :utc_datetime_usec))
+    drop unique_index(:rule_implementations, [:implementation_key])
+    create unique_index(:rule_implementations, [:implementation_key, :deleted_at])
+
+  end
+
+  def down do
+    alter(table(:rule_implementations), do: remove(:deleted_at))
+    drop unique_index(:rule_implementations, [:implementation_key, :deleted_at])
+    create unique_index(:rule_implementations, [:implementation_key])
+  end
+end
