793e83a0910d36c881370c83ad192c2186df614e
ivlo <ivan.lopez@bluetab.net>
2019-07-24 09:44:01 +0200

    chore: v3.2.0

--- mix.exs
+++ mix.exs
@@ -6,7 +6,7 @@ defmodule TdDd.Mixfile do
       app: :td_dd,
       version:
         case System.get_env("APP_VERSION") do
-          nil -> "3.1.0-local"
+          nil -> "3.2.0-local"
           v -> v
         end,
       elixir: "~> 1.6",
