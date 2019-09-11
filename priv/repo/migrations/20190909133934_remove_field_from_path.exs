365953bcb964cd3ad66ae089de6ca33fc1040a53
saul.isern <saul.isern@bluetab.net>
2019-09-11 15:38:26 +0200

    fix: [TD-2095] change implementations which rule is not deleted and filer system params keys being maps

--- priv/repo/migrations/20190909133934_remove_field_from_path.exs
+++ priv/repo/migrations/20190909133934_remove_field_from_path.exs
@@ -7,8 +7,10 @@ defmodule TdDq.Repo.Migrations.RemoveFieldFromPath do
 
   def change do
     from(ri in "rule_implementations")
-    |> where([ri], is_nil(ri.deleted_at))
-    |> select([ri], %{id: ri.id, system_params: ri.system_params})
+    |> join(:inner, [ri, r], r in "rules", on: r.id == ri.rule_id)
+    |> where([ri, _], is_nil(ri.deleted_at))
+    |> where([_, r], is_nil(r.deleted_at))
+    |> select([ri, _], %{id: ri.id, system_params: ri.system_params})
     |> Repo.all()
     |> Enum.filter(&with_path/1)
     |> Enum.map(&delete_field/1)
@@ -27,13 +29,12 @@ defmodule TdDq.Repo.Migrations.RemoveFieldFromPath do
   end
 
   defp field_in_path(system_params) do
-    Enum.filter(
-      system_params,
-      fn {_, value} ->
-        name = Map.get(value, "name")
-        Map.has_key?(value, "path") and name in Enum.take(Map.get(value, "path"), -1)
-      end
-    )
+    system_params
+    |> Enum.filter(fn {_, value} -> is_map(value) end)
+    |> Enum.filter(fn {_, value} ->
+      name = Map.get(value, "name")
+      Map.has_key?(value, "path") and name in Enum.take(Map.get(value, "path"), -1)
+    end)
   end
 
   defp delete_field(%{id: id, system_params: system_params}) do
@@ -55,10 +56,10 @@ defmodule TdDq.Repo.Migrations.RemoveFieldFromPath do
     %{id: id, system_params: params}
   end
 
-  defp update_system_params(%{id: id, system_params: system_params}) do 
+  defp update_system_params(%{id: id, system_params: system_params}) do
     from(ri in "rule_implementations")
-      |> update([ri], set: [system_params: ^system_params])
-      |> where([ri], ri.id == ^id)
-      |> Repo.update_all([])
+    |> update([ri], set: [system_params: ^system_params])
+    |> where([ri], ri.id == ^id)
+    |> Repo.update_all([])
   end
 end
