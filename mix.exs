372e932e7dbc29af2474becd7dc6bfa243bfca95
saul.isern <saul.isern@bluetab.net>
2019-10-02 17:44:30 +0200

    chore: bump cache version

--- mix.exs
+++ mix.exs
@@ -58,7 +58,7 @@ defmodule TdDq.Mixfile do
       {:canada, "~> 1.0.1"},
       {:elasticsearch, git: "https://github.com/Bluetab/elasticsearch-elixir.git", branch: "feature/encoding-opts"},
       {:td_hypermedia, git: "https://github.com/Bluetab/td-hypermedia.git", tag: "3.6.1"},
-      {:td_cache, git: "https://github.com/Bluetab/td-cache.git", tag: "3.7.1"},
+      {:td_cache, git: "https://github.com/Bluetab/td-cache.git", tag: "3.7.2"},
       {:td_df_lib, git: "https://github.com/Bluetab/td-df-lib.git", tag: "3.5.0"}
     ]
   end
