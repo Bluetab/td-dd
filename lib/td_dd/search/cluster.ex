80b94c621fb44e4509a55f339e235ff08e52fa35
Tom Crossland <tom.crossland@bluetab.net>
2019-10-04 16:38:04 +0200

    fix: prepend application path to settings

--- lib/td_dd/search/cluster.ex
+++ lib/td_dd/search/cluster.ex
@@ -2,4 +2,19 @@ defmodule TdDd.Search.Cluster do
   @moduledoc "Elasticsearch cluster configuration for TdDd"
 
   use Elasticsearch.Cluster, otp_app: :td_dd
+
+  def init(config) do
+    indexes =
+      config
+      |> Map.get(:indexes)
+      |> Enum.map(&prepend/1)
+      |> Map.new()
+
+    {:ok, %{config | indexes: indexes}}
+  end
+
+  defp prepend({index, %{settings: settings} = config}) do
+    settings = Path.join(Application.app_dir(:td_dd), settings)
+    {index, Map.put(config, :settings, settings)}
+  end
 end
