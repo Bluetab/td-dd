f2ba9b7e551ede01c19311c3d317e2dce567485a
Tom Crossland <tom.crossland@bluetab.net>
2019-10-11 00:47:52 +0200

    chore: bump td-cache to 3.8.0

--- mix.exs
+++ mix.exs
@@ -59,7 +59,7 @@ defmodule TdDd.Mixfile do
       {:elasticsearch,
        git: "https://github.com/Bluetab/elasticsearch-elixir.git",
        branch: "feature/bulk-index-action"},
-      {:td_cache, git: "https://github.com/Bluetab/td-cache.git", tag: "3.7.2"},
+      {:td_cache, git: "https://github.com/Bluetab/td-cache.git", tag: "3.8.0"},
       {:td_hypermedia, git: "https://github.com/Bluetab/td-hypermedia.git", tag: "3.6.1"},
       {:td_df_lib, git: "https://github.com/Bluetab/td-df-lib.git", tag: "3.5.0"}
     ]
