d364e1d1c8cf8290ebf9af4026eea213dfe8e304
Saul Isern <saul.isern@bluetab.net>
2020-03-25 15:39:09 +0100

    chore: [TD-2364] update cache

--- mix.exs
+++ mix.exs
@@ -57,7 +57,7 @@ defmodule TdDq.Mixfile do
        git: "https://github.com/Bluetab/elasticsearch-elixir.git",
        branch: "feature/bulk-index-action"},
       {:td_hypermedia, git: "https://github.com/Bluetab/td-hypermedia.git", tag: "3.6.1"},
-      {:td_cache, git: "https://github.com/Bluetab/td-cache.git", tag: "feature/td-2364"},
+      {:td_cache, git: "https://github.com/Bluetab/td-cache.git", branch: "feature/td-2364"},
       {:td_df_lib, git: "https://github.com/Bluetab/td-df-lib.git", tag: "3.14.0"},
       {:poison, "~> 3.1"}
     ]
