3580cfeaff964332bb4538c07544b8be10dae3b9
Saul Isern <saul.isern@bluetab.net>
2020-06-26 10:21:12 +0200

    feat: [TD-911] use traversals limiting graphs depth

--- mix.exs
+++ mix.exs
@@ -71,7 +71,7 @@ defmodule TdDd.Mixfile do
       {:td_cache, git: "https://github.com/Bluetab/td-cache.git", tag: "3.24.3"},
       {:td_hypermedia, git: "https://github.com/Bluetab/td-hypermedia.git", tag: "3.6.1"},
       {:td_df_lib, git: "https://github.com/Bluetab/td-df-lib.git", tag: "3.25.0"},
-      {:graph, git: "https://github.com/Bluetab/graph.git", tag: "0.1.1"}
+      {:graph, git: "https://github.com/Bluetab/graph.git", branch: "feature/td-911"}
     ]
   end
 
