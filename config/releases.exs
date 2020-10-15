28178eb4346cefb587581669fce56e5894c4a826
Tom Crossland <tom.crossland@bluetab.net>
2020-10-15 14:55:46 +0200

    feat: [TD-3058] allow database connection timeout to be configured

--- config/releases.exs
+++ config/releases.exs
@@ -6,7 +6,8 @@ config :td_dd, TdDd.Repo,
   password: System.fetch_env!("DB_PASSWORD"),
   database: System.fetch_env!("DB_NAME"),
   hostname: System.fetch_env!("DB_HOST"),
-  pool_size: System.get_env("DB_POOL_SIZE", "8") |> String.to_integer()
+  pool_size: System.get_env("DB_POOL_SIZE", "8") |> String.to_integer(),
+  timeout: System.get_env("DB_TIMEOUT_MILLIS", "600000") |> String.to_integer()
 
 config :td_dd, TdDd.Auth.Guardian, secret_key: System.fetch_env!("GUARDIAN_SECRET_KEY")
 
