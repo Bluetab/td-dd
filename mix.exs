c051c89ca0f93ff80cce42547b43f84845e28901
Saul Isern <saul.isern@bluetab.net>
2020-11-19 12:42:59 +0100

    feat: [TD-2486] search and download modificactions for template field of type domain

--- mix.exs
+++ mix.exs
@@ -71,8 +71,8 @@ defmodule TdDd.Mixfile do
        git: "https://github.com/Bluetab/elasticsearch-elixir.git",
        branch: "feature/bulk-index-action"},
       {:td_hypermedia, git: "https://github.com/Bluetab/td-hypermedia.git", tag: "4.0.0"},
-      {:td_cache, git: "https://github.com/Bluetab/td-cache.git", tag: "4.7.1"},
-      {:td_df_lib, git: "https://github.com/Bluetab/td-df-lib.git", tag: "4.9.0"},
+      {:td_cache, git: "https://github.com/Bluetab/td-cache.git", branch: "feature/td-2486"},
+      {:td_df_lib, git: "https://github.com/Bluetab/td-df-lib.git", branch: "feature/td-2486"},
       {:graph, git: "https://github.com/Bluetab/graph.git", tag: "1.0.0"}
     ]
   end
