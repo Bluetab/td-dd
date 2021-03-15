e880930cbcedb19ba98f9a630e4f0413e44a43ad
netto <euclydes.young@gmail.com>
2021-03-15 12:32:32 +0100

    refactor: [TD-3421] remove source_alias endpoint

--- config/test.exs
+++ config/test.exs
@@ -2,7 +2,9 @@ use Mix.Config
 
 # We don't run a server during test. If one is required,
 # you can enable the server option below.
-config :td_dd, TdDdWeb.Endpoint, server: true
+config :td_dd, TdDdWeb.Endpoint,
+  http: [port: 4105],
+  server: true
 
 # Print only warnings and errors during test
 config :logger, level: :warn
