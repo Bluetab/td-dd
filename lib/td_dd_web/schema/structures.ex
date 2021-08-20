4f2b3489985005009279a619111d21a38c2718b2
Tom Crossland <tom.crossland@bluetab.net>
2021-08-20 19:58:16 +0200

    feat: TD-3484 GraphQL API on /api/v2

---
+++ lib/td_dd_web/schema/structures.ex
@@ -0,0 +1,55 @@
+defmodule TdDdWeb.Schema.Structures do
+  @moduledoc """
+  Absinthe schema definitions for data structures and related entities.
+  """
+
+  use Absinthe.Schema.Notation
+
+  import Absinthe.Resolution.Helpers, only: [dataloader: 1]
+
+  alias TdDdWeb.Resolvers
+
+  object :structure_queries do
+    @desc "Get a list of data structure versions"
+    field :data_structure_versions, list_of(:data_structure_version) do
+      arg(:since, :datetime)
+      arg(:min_id, :integer)
+      arg(:limit, :integer, default_value: 1_000)
+      arg(:order_by, :string, default_value: "id")
+      resolve(&Resolvers.Structures.data_structure_versions/3)
+    end
+  end
+
+  object :data_structure_version do
+    field :id, non_null(:id)
+    field :version, non_null(:integer)
+    field :class, :string
+    field :description, :string
+    field :type, :string
+    field :group, :string
+    field :deleted_at, :datetime
+    field :inserted_at, :datetime
+    field :updated_at, :datetime
+    field :metadata, :json
+    field :data_structure, :data_structure, resolve: dataloader(TdDd.DataStructures)
+  end
+
+  object :data_structure do
+    field :id, non_null(:id)
+    field :confidential, non_null(:boolean)
+    field :domain_id, non_null(:integer)
+    field :external_id, non_null(:string)
+    field :inserted_at, :datetime
+    field :updated_at, :datetime
+    field :system, :system, resolve: dataloader(TdDd.DataStructures)
+  end
+
+  object :system do
+    field :id, non_null(:id)
+    field :external_id, non_null(:string)
+    field :name, non_null(:string)
+    field :df_content, :json
+    field :inserted_at, :datetime
+    field :updated_at, :datetime
+  end
+end
