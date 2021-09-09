351c7fe8eaa3820f73d7793a3b568658c24a7fb6
Saul Isern <saul.isern@bluetab.net>
2021-09-09 14:25:42 +0200

    refactor: table name should be a string

--- priv/repo/migrations/20210908131058_create_grant_approvers.exs
+++ priv/repo/migrations/20210908131058_create_grant_approvers.exs
@@ -2,12 +2,12 @@ defmodule TdDd.Repo.Migrations.CreateGrantApprovers do
   use Ecto.Migration
 
   def change do
-    create table(:grant_approvers) do
+    create table("grant_approvers") do
       add :name, :string, null: false
 
       timestamps()
     end
 
-    create unique_index(:grant_approvers, [:name])
+    create unique_index("grant_approvers", [:name])
   end
 end
