67d70c4a2ba25e690d16c3ff4bcc33ad6a641f4b
Tom Crossland <tom.crossland@bluetab.net>
2021-09-23 23:39:11 +0200

    refactor: split Grants context into Grants and Requests

--- lib/td_dd_web/controllers/approval_controller.ex
+++ lib/td_dd_web/controllers/approval_controller.ex
@@ -3,15 +3,15 @@ defmodule TdDdWeb.ApprovalController do
 
   import Canada, only: [can?: 2]
 
-  alias TdDd.Grants
+  alias TdDd.Grants.Requests
 
   action_fallback TdDdWeb.FallbackController
 
   def create(conn, %{"grant_request_id" => id, "approval" => params}) do
     with claims <- conn.assigns[:current_resource],
-         request <- Grants.get_grant_request!(id),
+         request <- Requests.get_grant_request!(id),
          {:can, true} <- {:can, can?(claims, approve(request))},
-         {:ok, %{approval: approval}} <- Grants.create_approval(claims, request, params) do
+         {:ok, %{approval: approval}} <- Requests.create_approval(claims, request, params) do
       conn
       |> put_status(:created)
       # |> put_resp_header("location", Routes.grant_request_approval_path(conn, :show, approval))
--- lib/td_dd_web/controllers/grant_request_controller.ex
+++ lib/td_dd_web/controllers/grant_request_controller.ex
@@ -3,14 +3,14 @@ defmodule TdDdWeb.GrantRequestController do
 
   import Canada, only: [can?: 2]
 
-  alias TdDd.Grants
   alias TdDd.Grants.GrantRequest
+  alias TdDd.Grants.Requests
 
   action_fallback TdDdWeb.FallbackController
 
   def index(conn, %{"grant_request_group_id" => id}) do
     with claims <- conn.assigns[:current_resource],
-         %{requests: requests} = group <- Grants.get_grant_request_group!(id),
+         %{requests: requests} = group <- Requests.get_grant_request_group!(id),
          {:can, true} <- {:can, can?(claims, show(group))} do
       render(conn, "index.json", grant_requests: requests)
     end
@@ -19,7 +19,7 @@ defmodule TdDdWeb.GrantRequestController do
   def index(conn, %{} = params) do
     with claims <- conn.assigns[:current_resource],
          {:can, true} <- {:can, can?(claims, list(GrantRequest))},
-         {:ok, grant_requests} <- Grants.list_grant_requests(claims, params) do
+         {:ok, grant_requests} <- Requests.list_grant_requests(claims, params) do
       render(conn, "index.json", grant_requests: grant_requests)
     end
   end
@@ -27,17 +27,17 @@ defmodule TdDdWeb.GrantRequestController do
   def show(conn, %{"id" => id}) do
     with claims <- conn.assigns[:current_resource],
          {:can, true} <- {:can, can?(claims, show(GrantRequest))} do
-      grant_request = Grants.get_grant_request!(id)
+      grant_request = Requests.get_grant_request!(id)
       render(conn, "show.json", grant_request: grant_request)
     end
   end
 
   def delete(conn, %{"id" => id}) do
-    grant_request = Grants.get_grant_request!(id)
+    grant_request = Requests.get_grant_request!(id)
 
     with claims <- conn.assigns[:current_resource],
          {:can, true} <- {:can, can?(claims, delete(GrantRequest))},
-         {:ok, %GrantRequest{}} <- Grants.delete_grant_request(grant_request) do
+         {:ok, %GrantRequest{}} <- Requests.delete_grant_request(grant_request) do
       send_resp(conn, :no_content, "")
     end
   end
