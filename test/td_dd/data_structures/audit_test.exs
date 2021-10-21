5786420a770282d7373bf727e173639bef542ec5
Julio Barriuso <julio.barriuso@bluetab.net>
2021-10-21 16:46:48 +0200

    fix: [TD-4211]: Subscriptions on data structures should include structure note events   * Set data_structure_note resource_type for structure_note_updated   * Add data_structure_id to publish payload

--- test/td_dd/data_structures/audit_test.exs
+++ test/td_dd/data_structures/audit_test.exs
@@ -5,23 +5,153 @@ defmodule TdDd.DataStructures.AuditTest do
   alias TdCache.Redix.Stream
   alias TdDd.DataStructures.Audit
   alias TdDd.DataStructures.DataStructure
+  alias TdDd.DataStructures.StructureNote
   alias TdDd.Repo
 
   @moduletag sandbox: :shared
   @stream TdCache.Audit.stream()
+  @template_name "structure_note_test_template"
 
   setup do
-    %{name: type} = CacheHelpers.insert_template()
+    %{id: template_id, name: template_name} = CacheHelpers.insert_template(name: @template_name)
+    CacheHelpers.insert_structure_type(name: template_name, template_id: template_id)
     start_supervised!(TdDd.Search.StructureEnricher)
     on_exit(fn -> Redix.del!(@stream) end)
 
     claims = build(:claims, role: "admin")
-    %{data_structure: data_structure} = insert(:data_structure_version, type: type)
-    [claims: claims, data_structure: data_structure, type: type]
+    data_structure = insert(:data_structure)
+    data_structure_version = insert(:data_structure_version, data_structure: data_structure, type: @template_name)
+    [claims: claims, data_structure_version: data_structure_version, type: @template_name]
+  end
+
+  describe "structure_note_updated/4" do
+    test "publishes an event", %{data_structure_version: data_structure_version, claims: %{user_id: user_id}} do
+
+      %{data_structure: %{id: data_structure_id} = data_structure} = data_structure_version
+
+      %{id: note_id} = note = insert(:structure_note,
+        data_structure: data_structure,
+        df_content: %{"string" => "initial", "list" => "one", "foo" => "bar"},
+        status: :draft,
+        version: 1
+      )
+
+      changeset = StructureNote.changeset(note, %{
+        df_content: %{"string" => "changed", "list" => "two", "foo" => "baz"}
+      })
+
+      assert {:ok, event_id} =
+        Audit.structure_note_updated(
+          Repo,
+          %{structure_note: note, latest: data_structure_version},
+          changeset,
+          user_id
+        )
+
+      assert {:ok, [event]} = Stream.range(:redix, @stream, event_id, event_id, transform: :range)
+
+      user_id = "#{user_id}"
+      resource_id = "#{note_id}"
+
+      assert %{
+                event: "structure_note_updated",
+                payload: payload,
+                resource_id: ^resource_id,
+                resource_type: "data_structure_note",
+                service: "td_dd",
+                ts: _ts,
+                user_id: ^user_id
+              } = event
+
+      assert %{
+                "data_structure_id" => ^data_structure_id
+              } = Jason.decode!(payload)
+    end
+  end
+
+  describe "structure_note_status_updated/4" do
+    test "publishes an event", %{data_structure_version: data_structure_version, claims: %{user_id: user_id}} do
+
+      %{data_structure: %{id: data_structure_id} = data_structure} = data_structure_version
+
+      %{id: note_id} = note = insert(:structure_note,
+        data_structure: data_structure,
+        df_content: %{"string" => "initial", "list" => "one", "foo" => "bar"},
+        status: :draft
+      )
+
+      assert {:ok, event_id} =
+        Audit.structure_note_status_updated(
+          Repo,
+          %{structure_note: note, latest: data_structure_version},
+          "pending_approval",
+          user_id
+        )
+
+      assert {:ok, [event]} = Stream.range(:redix, @stream, event_id, event_id, transform: :range)
+
+      user_id = "#{user_id}"
+      resource_id = "#{note_id}"
+
+      assert %{
+                event: "structure_note_pending_approval",
+                payload: payload,
+                resource_id: ^resource_id,
+                resource_type: "data_structure_note",
+                service: "td_dd",
+                ts: _ts,
+                user_id: ^user_id
+              } = event
+
+      assert %{
+                "data_structure_id" => ^data_structure_id
+              } = Jason.decode!(payload)
+    end
+  end
+
+  describe "structure_note_deleted/3" do
+    test "publishes an event", %{data_structure_version: data_structure_version, claims: %{user_id: user_id}} do
+
+      %{data_structure: %{id: data_structure_id} = data_structure} = data_structure_version
+
+      %{id: note_id} = note = insert(:structure_note,
+        data_structure: data_structure,
+        df_content: %{"string" => "initial", "list" => "one", "foo" => "bar"},
+        status: :published
+      )
+
+      assert {:ok, event_id} =
+        Audit.structure_note_deleted(
+          Repo,
+          %{structure_note: note, latest: data_structure_version},
+          user_id
+        )
+
+      assert {:ok, [event]} = Stream.range(:redix, @stream, event_id, event_id, transform: :range)
+
+      user_id = "#{user_id}"
+      resource_id = "#{note_id}"
+
+      assert %{
+                event: "structure_note_deleted",
+                payload: payload,
+                resource_id: ^resource_id,
+                resource_type: "data_structure_note",
+                service: "td_dd",
+                ts: _ts,
+                user_id: ^user_id
+              } = event
+
+      assert %{
+                "data_structure_id" => ^data_structure_id
+              } = Jason.decode!(payload)
+    end
   end
 
   describe "data_structure_updated/4" do
-    test "publishes an event", %{data_structure: data_structure, claims: %{user_id: user_id}} do
+    test "publishes an event", %{data_structure_version: data_structure_version, claims: %{user_id: user_id}} do
+
+      %{data_structure: %{id: data_structure_id} = data_structure} = data_structure_version
       %{id: data_structure_id} = data_structure
 
       params = %{confidential: true}
