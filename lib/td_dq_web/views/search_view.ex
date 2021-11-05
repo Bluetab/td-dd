15d4ee4f964ae24ca6fcacdb12a24bd951a27612
Guillermo Rodriguez <guillermo.r.navarrete@bluetab.net>
2021-11-05 10:20:36 +0100

    chore: [TD-4216] search view code enhancement

--- lib/td_dq_web/views/search_view.ex
+++ lib/td_dq_web/views/search_view.ex
@@ -17,25 +17,35 @@ defmodule TdDqWeb.SearchView do
     }
   end
 
-  def render("search.json", %{
-        implementations: implementations,
-        scroll_id: scroll_id
-      }) do
+  def render(
+        "search.json",
+        %{
+          implementations: implementations
+        } = assigns
+      ) do
     %{
-      data: render_many(implementations, ImplementationView, "implementation.json"),
-      scroll_id: scroll_id
+      data: render_many(implementations, ImplementationView, "implementation.json")
     }
+    |> with_user_permissions(assigns)
+    |> with_scroll_id(assigns)
+    |> with_filters(assigns)
   end
 
-  def render("search.json", %{
-        implementations: implementations,
-        filters: filters,
-        user_permissions: user_permissions
-      }) do
-    %{
-      data: render_many(implementations, ImplementationView, "implementation.json"),
-      filters: filters,
-      user_permissions: user_permissions
-    }
+  defp with_user_permissions(payload, %{user_permissions: user_permissions}) do
+    Map.put(payload, :user_permissions, user_permissions)
   end
+
+  defp with_user_permissions(payload, _assigns), do: payload
+
+  defp with_scroll_id(payload, %{scroll_id: scroll_id}) do
+    Map.put(payload, :scroll_id, scroll_id)
+  end
+
+  defp with_scroll_id(payload, _assigns), do: payload
+
+  defp with_filters(payload, %{filters: filters}) do
+    Map.put(payload, :filters, filters)
+  end
+
+  defp with_filters(payload, _assigns), do: payload
 end
