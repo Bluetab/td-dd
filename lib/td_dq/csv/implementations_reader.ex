40bc161934b2ffb3b9361bbdc1fcddfc48f9e266
Guillermo Rodriguez <guillermo.r.navarrete@bluetab.net>
2021-12-10 13:42:04 +0100

    chore: [TD-4301] fix credo issues

--- lib/td_dq/csv/implementations_reader.ex
+++ lib/td_dq/csv/implementations_reader.ex
@@ -2,10 +2,10 @@ defmodule TdDq.CSV.ImplementationsReader do
   @moduledoc """
   Module to read specific implementations CSV
   """
-
+  alias TdDq.CSV.Reader
   alias TdDq.Implementations.BulkLoad
 
   def read_csv(claims, stream) do
-    TdDq.CSV.Reader.read_csv(claims, stream, BulkLoad.required_headers(), &BulkLoad.bulk_load/2)
+    Reader.read_csv(claims, stream, BulkLoad.required_headers(), &BulkLoad.bulk_load/2)
   end
 end
