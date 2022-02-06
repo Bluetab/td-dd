5c5c862ba1f5897617b42b76f23c33a760e3a567
Julio Barriuso <julio.barriuso@bluetab.net>
2022-02-06 12:18:20 +0100

    chore: [TD-4456] Remove change unrelated to this task

--- lib/td_dd_web/controllers/lineage_event_controller.ex
+++ lib/td_dd_web/controllers/lineage_event_controller.ex
@@ -5,7 +5,6 @@ defmodule TdDdWeb.LineageEventController do
   import Canada, only: [can?: 2]
 
   alias TdDd.Lineage.LineageEvents
-  alias TdDd.Lineage.Units.Unit
 
   action_fallback(TdDdWeb.FallbackController)
 
@@ -21,8 +20,8 @@ defmodule TdDdWeb.LineageEventController do
   def index(conn, _params) do
     claims = conn.assigns[:current_resource]
 
-    with %{user_id: user_id} = _claims <- conn.assigns[:current_resource] do
-         #{:can, true} <- {:can, can?(claims, view_lineage)} |> IO.inspect(label: "LINEAGE_EVENT") do
+    with %{user_id: user_id} = _claims <- conn.assigns[:current_resource],
+         {:can, true} <- {:can, can?(claims, list(Lineage))} do
       render(conn, "index.json", %{lineage_events: LineageEvents.get_by_user_id(user_id)})
     end
   end
