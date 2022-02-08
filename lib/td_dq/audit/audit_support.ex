13f4a97504d431d8282a58124f6ed13af7529de2
Guillermo Rodriguez <guillermo.r.navarrete@bluetab.net>
2022-02-08 15:28:19 +0100

    change: [TD-4463] Audits rule_created and implementation_created for events notificvations

--- lib/td_dq/audit/audit_support.ex
+++ lib/td_dq/audit/audit_support.ex
@@ -41,7 +41,7 @@ defmodule TdDq.Audit.AuditSupport do
 
   defp payload(%{df_content: new_content}, %Implementation{df_content: old_content} = _data) do
     diff = MapDiff.diff(old_content, new_content, mask: &Masks.mask/1)
-    %{df_content: diff}
+    %{content: diff}
   end
 
   defp payload(%{df_content: new_content} = changes, %{df_content: old_content} = _data) do
