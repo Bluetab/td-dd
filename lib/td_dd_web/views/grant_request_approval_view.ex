8c92eabc3738487abef3dbf887387722109cd598
Tom Crossland <tom.crossland@bluetab.net>
2022-03-17 18:04:37 +0100

    refactor: pattern match on embeddings

--- lib/td_dd_web/views/grant_request_approval_view.ex
+++ lib/td_dd_web/views/grant_request_approval_view.ex
@@ -15,20 +15,14 @@ defmodule TdDdWeb.GrantRequestApprovalView do
 
   defp put_embeddings(%{} = resp, approval) do
     case embeddings(approval) do
-      map when map == %{} -> resp
-      embeddings -> Map.put(resp, :_embedded, embeddings)
+      :none -> resp
+      %{} = embeddings -> Map.put(resp, :_embedded, embeddings)
     end
   end
 
-  defp embeddings(%{} = approval) do
-    approval
-    |> Map.take([:user])
-    |> Enum.reduce(%{}, fn
-      {:user, %{} = user}, acc ->
-        Map.put(acc, :user, render_one(user, UserView, "embedded.json"))
-
-      _, acc ->
-        acc
-    end)
+  defp embeddings(%{user: user} = _approval) do
+    %{user: render_one(user, UserView, "embedded.json")}
   end
+
+  defp embeddings(_approval), do: :none
 end
