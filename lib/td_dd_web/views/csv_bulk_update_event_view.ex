49836dd1bfc510ebd2a3f384905e559fb1ebdb47
julio.barriuso@bluetab.net <julio.barriuso@bluetab.net>
2022-05-05 12:04:59 +0000

    TD-4586 Asynchronous CSV bulk update

---
+++ lib/td_dd_web/views/csv_bulk_update_event_view.ex
@@ -0,0 +1,19 @@
+defmodule TdDdWeb.CsvBulkUpdateEventView do
+  use TdDdWeb, :view
+
+  def render("index.json", %{csv_bulk_update_events: events}) do
+    render_many(events, __MODULE__, "show.json")
+  end
+
+  def render("show.json", %{csv_bulk_update_event: event}) do
+    Map.take(event, [
+      :user_id,
+      :response,
+      :csv_hash,
+      :status,
+      :task_reference,
+      :message,
+      :inserted_at
+    ])
+  end
+end
