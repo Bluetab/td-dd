b92d189ba7677be6a8cef40ffc2e120bb009117f
Guillermo Rodriguez <guillermo.r.navarrete@bluetab.net>
2022-05-05 10:21:07 +0200

    format: TD-4538 format document

--- priv/repo/migrations/20220505081053_chnage_reference_parent_id_rule_results.exs
+++ priv/repo/migrations/20220505081053_chnage_reference_parent_id_rule_results.exs
@@ -1,9 +1,9 @@
 defmodule TdDd.Repo.Migrations.ChnageReferenceParentIdRuleResults do
   use Ecto.Migration
 
-
   def up do
     alter table("rule_results"), do: remove(:parent_id)
+
     alter table("rule_results") do
       add(:parent_id, references("rule_results", on_delete: :delete_all))
     end
@@ -11,10 +11,9 @@ defmodule TdDd.Repo.Migrations.ChnageReferenceParentIdRuleResults do
 
   def down do
     alter table("rule_results"), do: remove(:parent_id)
+
     alter table("rule_results") do
       add(:parent_id, references("rule_results", on_delete: :delete_all))
     end
   end
-
-
 end
