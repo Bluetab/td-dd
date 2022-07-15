7fcd9f5af406deda6b921c79147e81e1158bb8d9
Tom Crossland <tom.crossland@bluetab.net>
2022-07-15 11:02:36 +0200

    chore: remove unused case clauses

--- lib/td_dd/data_structures/bulk_update.ex
+++ lib/td_dd/data_structures/bulk_update.ex
@@ -361,12 +361,6 @@ defmodule TdDd.DataStructures.BulkUpdate do
 
   defp reduce_notes_results(result, acc) do
     case result do
-      {:ok, %{structure_note_update: %{data_structure_id: id} = structure_note}} ->
-        {:cont, Map.put(acc, id, structure_note)}
-
-      {:ok, %{structure_note: %{data_structure_id: id} = structure_note}} ->
-        {:cont, Map.put(acc, id, structure_note)}
-
       {:ok, %{data_structure_id: id} = structure_note} ->
         {:cont, Map.put(acc, id, structure_note)}
 
