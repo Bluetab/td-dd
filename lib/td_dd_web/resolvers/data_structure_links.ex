3f845dfb2f7a5972bed26e839458ff372496953c
Julio Barriuso <julio.barriuso@bluetab.net>
2022-11-03 08:35:47 +0100

    chore: TD-5038 Unused variables

--- lib/td_dd_web/resolvers/data_structure_links.ex
+++ lib/td_dd_web/resolvers/data_structure_links.ex
@@ -6,11 +6,11 @@ defmodule TdDdWeb.Resolvers.DataStructureLinks do
 
   alias TdDd.DataStructures.DataStructureLinks
 
-  def data_structure_link(_data_structure_link, %{source_id: source_id, target_id: target_id} = params, _resolution) do
+  def data_structure_link(_data_structure_link, %{source_id: _source_id, target_id: _target_id} = params, _resolution) do
     {:ok, DataStructureLinks.get_by(params)}
   end
 
-  def data_structure_link(_data_structure_link, %{external_id: external_id} = params, _resolution) do
+  def data_structure_link(_data_structure_link, %{external_id: external_id}, _resolution) do
     {:ok, DataStructureLinks.all_by_external_id(external_id)}
   end
 
