af1eb5090bba6649e6161f6fd19d1c66a97c83c2
Guillermo Rodriguez <guillermo.r.navarrete@bluetab.net>
2022-11-10 13:15:22 +0100

    fix: add admin confitions for get roles

--- lib/td_dd_web/resolvers/me.ex
+++ lib/td_dd_web/resolvers/me.ex
@@ -15,30 +15,38 @@ defmodule TdDdWeb.Resolvers.Me do
 
   def roles(_parent, args, resolution) do
     case claims(resolution) do
-      %{user_id: id} -> {:ok, get_roles(id, args)}
+      %{} = claims -> {:ok, get_roles(claims, args)}
       _ -> {:error, :forbidden}
     end
   end
 
-  defp get_roles(user_id, %{domain_id: string_domain_id} = args)
+  defp get_roles(claims, %{domain_id: string_domain_id} = args)
        when is_binary(string_domain_id) do
     domain_id = String.to_integer(string_domain_id)
-    get_roles(user_id, Map.put(args, :domain_id, domain_id))
+    get_roles(claims, Map.put(args, :domain_id, domain_id))
   end
 
-  defp get_roles(user_id, args) do
-    user_roles =
-      user_id
-      |> UserCache.get_roles()
-      |> then(fn {:ok, roles} -> roles end)
-      |> Enum.filter(fn {_role, domain_ids} -> filter_roles_by_domain(domain_ids, args) end)
-      |> Enum.map(fn {role, _domain_ids} -> role end)
-      |> MapSet.new()
+  defp get_roles(claims, args) do
+    user_roles = get_user_roles(claims, args)
 
     args
     |> maybe_get_permission_roles()
     |> maybe_intersect_role_sets(user_roles)
     |> MapSet.to_list()
+  end
+
+  defp get_user_roles(%{role: "admin"}, _args), do: :all
+
+  defp get_user_roles(%{user_id: user_id}, args) do
+    user_id
+    |> UserCache.get_roles()
+    |> then(fn
+      {:ok, nil} -> %{}
+      {:ok, roles} -> roles
+    end)
+    |> Enum.filter(fn {_role, domain_ids} -> filter_roles_by_domain(domain_ids, args) end)
+    |> Enum.map(fn {role, _domain_ids} -> role end)
+    |> MapSet.new()
   end
 
   defp filter_roles_by_domain(domain_ids, %{domain_id: domain_id}),
@@ -55,6 +63,7 @@ defmodule TdDdWeb.Resolvers.Me do
 
   defp maybe_get_permission_roles(_), do: nil
 
+  defp maybe_intersect_role_sets(permission_roles, :all), do: permission_roles
   defp maybe_intersect_role_sets(nil, user_roles), do: user_roles
 
   defp maybe_intersect_role_sets(permission_roles, user_roles),
