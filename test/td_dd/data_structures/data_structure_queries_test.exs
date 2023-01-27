ad4d617fc929db3fe892b5029dfb5aeedb34b628
Juan Francisco Huete Verdejo <jf.huete@bluetab.net>
2023-01-27 09:34:42 +0100

    chore: TD-5493 Mix format

--- test/td_dd/data_structures/data_structure_queries_test.exs
+++ test/td_dd/data_structures/data_structure_queries_test.exs
@@ -95,13 +95,14 @@ defmodule TdDd.DataStructures.DataStructureQueriesTest do
     %{id: grant_id} = grant = insert(:grant, data_structure_id: dsv_a_ds_id)
 
     assert [
-      %{
-        dsv_children: dsv_children,
-        grant: ^grant
-      }
-    ] =
-    DataStructureQueries.children(%{grant_ids: [grant_id]})
-    |> Repo.all()
+             %{
+               dsv_children: dsv_children,
+               grant: ^grant
+             }
+           ] =
+             DataStructureQueries.children(%{grant_ids: [grant_id]})
+             |> Repo.all()
+
     assert dsv_children <|> [dsv_a_id, dsv_b_id, dsv_c_id]
   end
 
