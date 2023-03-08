0af31c5f472ff479a753fff897b9eb97e6fde213
Guillermo Rodriguez <guillermo.r.navarrete@bluetab.net>
2023-03-08 16:37:05 +0100

    feat: TD-3806 add cached content for implementations, rules, remediations plan and structures notes

--- lib/td_cx/format.ex
+++ lib/td_cx/format.ex
@@ -7,8 +7,11 @@ defmodule TdCx.Format do
 
   def get_cached_content(%{} = content, type) when is_binary(type) do
     case TemplateCache.get_by_name!(type) do
-      template = %{} -> Format.enrich_content_values(content, template, [:domain, :system])
-      _ -> content
+      template = %{} ->
+        Format.enrich_content_values(content, template, [:domain, :system, :hierarchy])
+
+      _ ->
+        content
     end
   end
 
