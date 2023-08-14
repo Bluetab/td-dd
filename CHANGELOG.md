5abe16f5567735b45988ecf9e73080ff30d153f5
julio.barriuso@bluetab.net <julio.barriuso@bluetab.net>
2023-08-14 11:28:00 +0000

    TD-2778: Indexing logging and error handling

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,8 +7,11 @@
 - [TD-5715] Add posibility for inherit domains when load new structures
 - [TD-5902] Ecto migration to remove index duplicity and reduce disk usage
 - [TD-5936] Add Note resolver for dataStructureVersion v2 queries
-- [TD-5937] Limit Catalog Metadata Views to data structures filters
 - [TD-5938] Enable Edit as default/raw implementation for draft status implementations
+- [TD-5937] Limit Catalog Metadata Views to data structures filters
+- [TD-2778] The following:
+  - Bulk and complete reindex: better logging.
+  - Complete reindex: delete temporal index if there are any errors.
 
 ### Fixed
 
--- lib/td_cx/search/indexer.ex
+++ lib/td_cx/search/indexer.ex
@@ -2,8 +2,8 @@ defmodule TdCx.Search.Indexer do
   @moduledoc """
   Manages elasticsearch indices
   """
-  alias Elasticsearch.Index
   alias Elasticsearch.Index.Bulk
+  alias Truedat.Search.Indexer
   alias TdCache.Redix
   alias TdCx.Jobs.Job
   alias TdCx.Search.Mappings
@@ -20,16 +20,8 @@ defmodule TdCx.Search.Indexer do
     Mappings.get_mappings()
     |> Map.put(:index_patterns, "#{alias_name}-*")
     |> Jason.encode!()
-    |> put_template(alias_name)
-    |> case do
-      {:ok, _} ->
-        Cluster
-        |> Index.hot_swap(alias_name)
-        |> log_errors()
-
-      error ->
-        error
-    end
+    |> Indexer.put_template(Cluster, alias_name)
+    |> Indexer.maybe_hot_swap(Cluster, alias_name)
   end
 
   def reindex(ids) do
@@ -42,7 +34,7 @@ defmodule TdCx.Search.Indexer do
       |> Stream.chunk_every(Cluster.setting(:jobs, :bulk_page_size))
       |> Stream.map(&Enum.join(&1, ""))
       |> Stream.map(&Elasticsearch.post(Cluster, "/#{alias_name}/_doc/_bulk", &1))
-      |> Stream.map(&log(&1, @action))
+      |> Stream.map(&Indexer.log_bulk_post(alias_name, &1, @action))
       |> Stream.run()
     end)
   end
@@ -59,9 +51,9 @@ defmodule TdCx.Search.Indexer do
   def verify_indices do
     alias_name = Cluster.alias_name(:jobs)
 
-    unless alias_exists?(alias_name) do
+    unless Indexer.alias_exists?(Cluster, alias_name) do
       if can_reindex?() do
-        delete_existing_index(alias_name)
+        Indexer.delete_existing_index(Cluster, alias_name)
 
         Timer.time(
           fn -> reindex(:all) end,
@@ -73,80 +65,8 @@ defmodule TdCx.Search.Indexer do
     end
   end
 
-  defp put_template(template, name) do
-    case Elasticsearch.put(Cluster, "/_template/#{name}", template,
-           params: %{"include_type_name" => "false"}
-         ) do
-      {:ok, res} ->
-        {:ok, res}
-
-      {:error, e} ->
-        Logger.warn("Error updating template #{name}: #{inspect(e)}")
-        {:error, e}
-    end
-  end
-
-  defp alias_exists?(name) do
-    case Elasticsearch.head(Cluster, "/_alias/#{name}") do
-      {:ok, _} -> true
-      {:error, _} -> false
-    end
-  end
-
-  defp delete_existing_index(name) do
-    case Elasticsearch.delete(Cluster, "/#{name}") do
-      {:ok, _} ->
-        Logger.info("Deleted index #{name}")
-
-      {:error, %{status: 404}} ->
-        :ok
-
-      error ->
-        error
-    end
-  end
-
   # Ensure only one instance of cx is reindexing by creating a lock in Redis
   defp can_reindex? do
     Redix.command!(["SET", "TdCx.Search.Indexer:LOCK", node(), "NX", "EX", 3600]) == "OK"
-  end
-
-  defp log({:ok, %{"errors" => false, "items" => items, "took" => took}}, _action) do
-    Logger.info("Indexed #{Enum.count(items)} documents (took=#{took})")
-  end
-
-  defp log({:ok, %{"errors" => true} = response}, action) do
-    first_error = response["items"] |> Enum.find(& &1[action]["error"])
-    Logger.warn("Bulk indexing encountered errors #{inspect(first_error)}")
-  end
-
-  defp log({:error, error}, _action) do
-    Logger.warn("Bulk indexing encountered errors #{inspect(error)}")
-  end
-
-  defp log(error, _action) do
-    Logger.warn("Bulk indexing encountered errors #{inspect(error)}")
-  end
-
-  defp log_errors(:ok), do: :ok
-  defp log_errors({:error, [e | _] = es}), do: log_errors(e, length(es))
-  defp log_errors({:error, e}), do: log_errors(e, 1)
-
-  defp log_errors(e, 1) do
-    message = message(e)
-    Logger.warn("Reindexing finished with error: #{message}")
-  end
-
-  defp log_errors(e, count) do
-    message = message(e)
-    Logger.warn("Reindexing finished with #{count} errors including: #{message}")
-  end
-
-  defp message(e) do
-    if Exception.exception?(e) do
-      Exception.message(e)
-    else
-      "#{inspect(e)}"
-    end
   end
 end
