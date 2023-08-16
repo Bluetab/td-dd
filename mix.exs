e4ca5d0702e232b8b0819d4759586ac14dc99782
Julio Barriuso <julio.barriuso@bluetab.net>
2023-08-16 16:57:59 +0200

    chore: TD-5948 version 5.12.0

--- mix.exs
+++ mix.exs
@@ -6,7 +6,7 @@ defmodule TdDd.MixProject do
       app: :td_dd,
       version:
         case System.get_env("APP_VERSION") do
-          nil -> "5.11.3-local"
+          nil -> "5.12.0-local"
           v -> v
         end,
       elixir: "~> 1.14",
