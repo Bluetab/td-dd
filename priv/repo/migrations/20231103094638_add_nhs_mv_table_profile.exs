191ae2cb3b54249f175b4d74d6fd73b3772f799a
FJavierHurtado <145583809+FJavierHurtado@users.noreply.github.com>
2023-11-13 16:00:34 +0100

    Update 20231103094638_add_nhs_mv_table_profile.exs

--- priv/repo/migrations/20231103094638_add_nhs_mv_table_profile.exs
+++ priv/repo/migrations/20231103094638_add_nhs_mv_table_profile.exs
@@ -49,22 +49,5 @@ defmodule TdDd.Repo.Migrations.AddNhsMvTableProfile do
       "DROP MATERIALIZED VIEW IF EXISTS vm_table_profile"
     )
 
-    # execute(
-    #   """
-    #   SELECT cron.schedule(
-    #   'vm_table_profile',
-    #   '*/5 * * * *',
-    #   'REFRESH MATERIALIZED VIEW public.vm_table_profile') 
-
-    #   """,
-    #   "SELECT cron.unschedule('vm_table_profile')"
-    # )
-
-    # execute(
-    #   """
-    #   UPDATE cron.job SET database = 'td_dd' WHERE jobid = (select jobid from cron.job where jobname  = 'vm_table_profile')
-
-    #   """
-    # )
   end
 end
