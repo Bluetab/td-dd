9d5358fe6b7a316bf1f4880f834fccca10661e55
Julio Barriuso <julio.barriuso@bluetab.net>
2023-11-28 17:51:40 +0100

    chore: TD-6185 version 5.19.0

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,6 +1,6 @@
 # Changelog
 
-## [Unreleased]
+## [5.19.0] 2023-11-28
 
 ### Added
 
--- mix.exs
+++ mix.exs
@@ -6,7 +6,7 @@ defmodule TdDd.MixProject do
       app: :td_dd,
       version:
         case System.get_env("APP_VERSION") do
-          nil -> "5.18.0-local"
+          nil -> "5.19.0-local"
           v -> v
         end,
       elixir: "~> 1.14",
