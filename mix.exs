b7c06a073552a78397f60964268dd9d2826cfec1
Guillermo RN <guillermo.r.navarrete@bluetab.net>
2023-12-22 12:50:35 +0000

    TD-6181 add some tests and td-core bump version

--- mix.exs
+++ mix.exs
@@ -84,7 +84,7 @@ defmodule TdDd.MixProject do
       {:td_df_lib, git: "https://github.com/Bluetab/td-df-lib.git", tag: "5.20.0"},
       {:td_cluster,
        git: "https://github.com/Bluetab/td-cluster.git", tag: "5.19.0", override: true},
-      {:td_core, git: "https://github.com/Bluetab/td-core.git", tag: "6.0.0"},
+      {:td_core, git: "https://github.com/Bluetab/td-core.git", tag: "6.0.1"},
       {:graph, git: "https://github.com/Bluetab/graph.git", tag: "1.3.0"},
       {:vaultex, "~> 1.0.1"},
       {:sobelow, "~> 0.11", only: [:dev, :test]}
