2fe39a3c0933f69a5a94b227debf72cbba691a4f
juancarlos.pazos@bluetab.net <juancarlos.pazos@bluetab.net>
2024-01-19 09:41:40 +0000

    TD-6275 Release 6.0.0

--- mix.exs
+++ mix.exs
@@ -6,7 +6,7 @@ defmodule TdDd.MixProject do
       app: :td_dd,
       version:
         case System.get_env("APP_VERSION") do
-          nil -> "5.20.0-local"
+          nil -> "6.0.0-local"
           v -> v
         end,
       elixir: "~> 1.14",
