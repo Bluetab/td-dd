155085e6375a78a5d013f9c5a6f8001806133ac5
javier.hurtado <javier.hurtado@bluetab.net>
2024-02-01 07:12:33 +0100

    feature:[EDAP-4.47+]undo migrations02_4.47.11

--- priv/repo/migrations/20220614181042_drop_implementation_ref_to_rule_implementations.exs
+++ priv/repo/migrations/20220614181042_drop_implementation_ref_to_rule_implementations.exs
@@ -1,19 +1,7 @@
 defmodule TdDd.Repo.Migrations.DropImplementationRefToRuleImplementations do
   use Ecto.Migration
 
-  @set_implementation_ref """
-  update rule_implementations riu set implementation_ref = subquery.implementation_ref
-  from
-    (select ri.id, fri.id as implementation_ref from rule_implementations ri
-      inner join
-      (
-        select ri2.id, ri2.implementation_key, ri2.version from rule_implementations ri2 where (implementation_key, version)
-        in (select implementation_key, min(version) from rule_implementations group by implementation_key)
-      ) as fri
-      on fri.implementation_key = ri.implementation_key
-    ) as subquery
-  where riu.id = subquery.id
-  """
+ 
 
   def change do
     alter table("rule_implementations") do
