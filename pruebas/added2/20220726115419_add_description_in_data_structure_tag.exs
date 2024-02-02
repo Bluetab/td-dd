890af631a526ad44917377ede4f3b30308693864
javier.hurtado <javier.hurtado@bluetab.net>
2024-02-02 08:41:05 +0100

    feature:[EDAP-4.47+]td-dd merged with TD 4.47.1 and 4.48 from TD4925 to END-008

--- pruebas/added2/20220726115419_add_description_in_data_structure_tag.exs
+++ pruebas/added2/20220726115419_add_description_in_data_structure_tag.exs
@@ -2,7 +2,7 @@ defmodule TdDd.Repo.Migrations.AddDescriptionInDataStructureTag do
   use Ecto.Migration
 
   def change do
-    alter table("data_structure_tags") do
+    alter table("data_structures_tags") do
       add :description, :string, size: 1_000, default: nil
     end
   end
