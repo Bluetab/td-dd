c4a08d0e70826710067098be2200f394a8c00a0b
jf.huete@bluetab.net <jf.huete@bluetab.net>
2024-04-10 10:53:10 +0000

    TD-6424: Fixed switch on fields aren't translated when uploading and downloading

--- mix.exs
+++ mix.exs
@@ -83,7 +83,8 @@ defmodule TdDd.MixProject do
       {:td_cache, git: "https://github.com/Bluetab/td-cache.git", tag: "6.3.0"},
       {:td_cluster,
        git: "https://github.com/Bluetab/td-cluster.git", tag: "5.19.0", override: true},
-      {:td_core, git: "https://github.com/Bluetab/td-core.git", tag: "6.4.1"},
+      {:td_core, git: "https://github.com/Bluetab/td-core.git", tag: "6.5.0"},
+      {:td_df_lib, git: "https://github.com/Bluetab/td-df-lib.git", tag: "6.5.0"},
       {:graph, git: "https://github.com/Bluetab/graph.git", tag: "1.3.0"},
       {:vaultex, "~> 1.0.1"},
       {:sobelow, "~> 0.11", only: [:dev, :test]}
