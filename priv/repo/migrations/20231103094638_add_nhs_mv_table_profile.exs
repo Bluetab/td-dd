32d0ad1b3d6d9584bce4add2a7e75a9868a5480c
daniel.anton@bluetab.net <daniel.anton@bluetab.net>
2024-05-16 15:37:22 +0200

    Fix typo

--- priv/repo/migrations/20231103094638_add_nhs_mv_table_profile.exs
+++ priv/repo/migrations/20231103094638_add_nhs_mv_table_profile.exs
@@ -32,7 +32,7 @@ defmodule TdDd.Repo.Migrations.AddNhsMvTableProfile do
       p.value::json->>'interquartile_range' as interquartile_range,
       p.value::json->>'missing_values_count' as missing_values_count,
       p.value::json->>'distinct_values_count' as distinct_values_count,
-      p.value::json->>'z_score_outliers_count' as z_score_outliers_count,
+      p.value::json->>'z_score_outliers_count' as z_score_outliners_count,
       p.value::json->>'median_absolute_deviation' as median_absolute_deviation,
       json_array_elements(
       CASE
