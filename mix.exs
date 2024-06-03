9e26e06cd51f3a2beadc4bc9fe0afd6136e6914e
Guillermo Rodriguez <guillermo.r.navarrete@bluetab.net>
2024-06-03 12:48:32 +0200

    chore: TD-6640 bump version 6.1.3

--- mix.exs
+++ mix.exs
@@ -6,7 +6,7 @@ defmodule TdDd.MixProject do
       app: :td_dd,
       version:
         case System.get_env("APP_VERSION") do
-          nil -> "6.1.1-local"
+          nil -> "6.1.3-local"
           v -> v
         end,
       elixir: "~> 1.14",
