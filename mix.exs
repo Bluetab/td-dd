90b948482dc8064689c055015231788eb265b09a
Alejandro Oliva López <alejandro.oliva@bluetab.net>
2024-07-29 10:16:59 +0000

    TD-6734 Update td-df-lib and td-core

--- mix.exs
+++ mix.exs
@@ -82,8 +82,8 @@ defmodule TdDd.MixProject do
       {:elasticsearch, "~> 1.1"},
       {:td_cache, git: "https://github.com/Bluetab/td-cache.git", tag: "6.9.2"},
       {:td_cluster, git: "https://github.com/Bluetab/td-cluster.git", tag: "5.19.0"},
-      {:td_df_lib, git: "https://github.com/Bluetab/td-df-lib.git", tag: "6.9.5"},
-      {:td_core, git: "https://github.com/Bluetab/td-core.git", tag: "6.9.5"},
+      {:td_df_lib, git: "https://github.com/Bluetab/td-df-lib.git", tag: "6.9.6"},
+      {:td_core, git: "https://github.com/Bluetab/td-core.git", tag: "6.9.6"},
       {:graph, git: "https://github.com/Bluetab/graph.git", tag: "1.3.0"},
       {:vaultex, "~> 1.0.1"},
       {:sobelow, "~> 0.11", only: [:dev, :test]}
