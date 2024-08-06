895d2da659143555b755cfb652a76dfcbbb56ea8
Lorenzo Sánchez Fraile <lorenzo.sanchez@bluetab.net>
2024-08-06 12:11:48 +0200

    feat: TD-6744 Added resolver for new field 'has_note' in data structure version v2 schema

--- lib/td_dd/data_structures/data_structures.ex
+++ lib/td_dd/data_structures/data_structures.ex
@@ -225,6 +225,8 @@ defmodule TdDd.DataStructures do
   end
 
   def get_cached_content(%{} = content, %{data_structure_type: %{template_id: template_id}}) do
+    IO.inspect(TemplateCache.get(template_id), label: "TemplateCache.get(template_id) => ")
+
     case TemplateCache.get(template_id) do
       {:ok, template} ->
         Format.enrich_content_values(content, template, [:system, :hierarchy])
