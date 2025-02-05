85b6fe6132fc8a02d53510ee46995065d7bd3dd0
Guillermo RN <guillermo.r.navarrete@bluetab.net>
2025-02-05 08:17:18 +0000

    TD-7087 update version 7.1.0

--- mix.exs
+++ mix.exs
@@ -6,7 +6,7 @@ defmodule TdDd.MixProject do
       app: :td_dd,
       version:
         case System.get_env("APP_VERSION") do
-          nil -> "7.0.0-local"
+          nil -> "7.1.0-local"
           v -> v
         end,
       elixir: "~> 1.18",
