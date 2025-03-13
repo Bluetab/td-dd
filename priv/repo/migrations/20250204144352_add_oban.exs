fbdfd216ecde35597c5b9a3651658d924b50edc0
Euclydes Netto <euclydes.young@bluetab.net>
2025-03-13 10:10:38 +0100

    feat: TD-7161 add create_schema flag on Oban migration

--- priv/repo/migrations/20250204144352_add_oban.exs
+++ priv/repo/migrations/20250204144352_add_oban.exs
@@ -1,7 +1,12 @@
 defmodule TdDd.Repo.Migrations.AddOban do
   use Ecto.Migration
 
-  def up, do: Oban.Migration.up(prefix: Application.get_env(:td_dd, Oban)[:prefix])
+  def up,
+    do:
+      Oban.Migration.up(
+        prefix: Application.get_env(:td_dd, Oban)[:prefix],
+        create_schema: Application.get_env(:td_dd, Oban)[:create_schema]
+      )
 
   def down,
     do: Oban.Migration.down(prefix: Application.get_env(:td_dd, Oban)[:prefix], version: 1)
