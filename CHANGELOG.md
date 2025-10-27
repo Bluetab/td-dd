0cc850b7a598f54e7af0c1133428cd4c20e39885
Guillermo Rodriguez <guillermo.r.navarrete@bluetab.net>
2025-10-27 13:40:21 +0100

    chore: TD-7617 bump version 7.11.2

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,6 +1,6 @@
 # Changelog
 
-## [Unreleased]
+## [7.11.2] 2025-10-27
 
 ### Fixed
 
--- mix.exs
+++ mix.exs
@@ -6,7 +6,7 @@ defmodule TdDd.MixProject do
       app: :td_dd,
       version:
         case System.get_env("APP_VERSION") do
-          nil -> "7.11.1-local"
+          nil -> "7.11.2-local"
           v -> v
         end,
       elixir: "~> 1.18",
